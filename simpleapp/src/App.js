f5cfd41fdc14db68af5a7e0d222e37e1121a07ca
florinbatog1999@gmail.com <florinbatog1999@gmail.com>
2020-10-14 14:58:46 +0300

    simpleapp commit

---
+++ simpleapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (//parantezele sunt folosite pentru a scrie si pe mai multe linii
+    <div>
+      Hello from React team!
+      </div>
+    )
+    
+
+  }
+}
+
+export default App;
